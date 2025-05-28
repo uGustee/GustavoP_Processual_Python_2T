cb0e99124394e6ba2ea1dd5b208f71d27171d299
gustavodap <72631936+uGustee@users.noreply.github.com>
2025-05-28 09:33:36 -0300

    Create numerosPrimos_eDivisiveis.py

---
+++ numerosPrimos_eDivisiveis.py
@@ -0,0 +1,148 @@
+for i in range(1, 101):
+    print (i)
+    if i == 2:
+        print ("número primo")
+    if i == 3:
+        print ("número primo")
+    if i == 5:
+        print ("número primo")
+    if i == 7:
+        print ("número primo")
+    if i == 11:
+        print ("número primo")
+    if i == 13:
+        print ("número primo")
+    if i == 17:
+        print ("número primo")
+    if i == 19:
+        print ("número primo")
+    if i == 23:
+        print ("número primo")
+    if i == 29:
+        print ("número primo")
+    if i == 31:
+        print ("número primo")
+    if i == 37:
+        print ("número primo")
+    if i == 41:
+        print ("número primo")
+    if i == 43:
+        print ("número primo")
+    if i == 47:
+        print ("número primo")
+    if i == 53:
+        print ("número primo")
+    if i == 59:
+        print ("número primo")
+    if i == 67:
+        print ("número primo")
+    if i == 71:
+        print ("número primo")
+    if i == 73:
+        print ("número primo")
+    if i == 79:
+        print ("número primo")
+    if i == 83:
+        print ("número primo")
+    if i == 89:
+        print ("número primo")
+    if i == 97:
+        print ("número primo")
+    if i == 4:
+        print ("número divisivel por 2")
+    if i == 6:
+        print ("número divisivel por 2")
+    if i == 8:
+        print ("número divisivel por 2")
+    if i == 10:
+        print ("número divisivel por 2")
+    if i == 12:
+        print ("número divisivel por 2")
+    if i == 14:
+        print ("número divisivel por 2")
+    if i == 16:
+        print ("número divisivel por 2")
+    if i == 18:
+        print ("número divisivel por 2")
+    if i == 20:
+        print ("número divisivel por 2")
+    if i == 22:
+        print ("número divisivel por 2")
+    if i == 24:
+        print ("número divisivel por 2")
+    if i == 26:
+        print ("número divisivel por 2")
+    if i == 28:
+        print ("número divisivel por 2")
+    if i == 30:
+        print ("número divisivel por 2")
+    if i == 32:
+        print ("número divisivel por 2")
+    if i == 34:
+        print ("número divisivel por 2")
+    if i == 36:
+        print ("número divisivel por 2")
+    if i == 38:
+        print ("número divisivel por 2")
+    if i == 40:
+        print ("número divisivel por 2")
+    if i == 42:
+        print ("número divisivel por 2")
+    if i == 44:
+        print ("número divisivel por 2")
+    if i == 46:
+        print ("número divisivel por 2")
+    if i == 48:
+        print ("número divisivel por 2")
+    if i == 50:
+        print ("número divisivel por 2")
+    if i == 52:
+        print ("número divisivel por 2")
+    if i == 54:
+        print ("número divisivel por 2")
+    if i == 56:
+        print ("número divisivel por 2")
+    if i == 58:
+        print ("número divisivel por 2")
+    if i == 60:
+        print ("número divisivel por 2")
+    if i == 62:
+        print ("número divisivel por 2")
+    if i == 64:
+        print ("número divisivel por 2")
+    if i == 66:
+        print ("número divisivel por 2")
+    if i == 68:
+        print ("número divisivel por 2")
+    if i == 70:
+        print ("número divisivel por 2")
+    if i == 72:
+        print ("número divisivel por 2")
+    if i == 74:
+        print ("número divisivel por 2")
+    if i == 76:
+        print ("número divisivel por 2")
+    if i == 78:
+        print ("número divisivel por 2")
+    if i == 80:
+        print ("número divisivel por 2")
+    if i == 82:
+        print ("número divisivel por 2")
+    if i == 84:
+        print ("número divisivel por 2")
+    if i == 86:
+        print ("número divisivel por 2")
+    if i == 88:
+        print ("número divisivel por 2")
+    if i == 90:
+        print ("número divisivel por 2")
+    if i == 92:
+        print ("número divisivel por 2")
+    if i == 94:
+        print ("número divisivel por 2")
+    if i == 96:
+        print ("número divisivel por 2")
+    if i == 98:
+        print ("número divisivel por 2")
+    if i == 100:
+        print ("número divisivel por 2")
